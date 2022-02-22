1c6db0474db950c39da90e7be2bd29efb916a1fb
kimmingyu <kimmin36944@gmail.com>
2022-02-23 03:29:25 +0900

    feat: navigation bar implementing(toggle button isn't yet implemented)

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,46 +1,34 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Button from '@mui/material/Button';
 import './Navbar.css'
 
-function Navbar() {
-    return (
-        <Box sx={{flexGrow: 1}}>
-            <AppBar style={{backgroundColor:'#000000'}} position="static">
+import Logo from './Sections/Logo';
+import RightSide from './Sections/RightSide';
+import Toggle from './Sections/Toggle';
 
-                <Toolbar>
+function Navbar(props) {
 
-                    <Button  color="inherit" variant="h6" component="div" sx={{flexGrow: 1}}>
-                        <a href="/">
-                            온라인 포커
-                        </a>
-                    </Button>
+    const [toggle,setToggle] =  useState(true)
+    
+    if(toggle){
+        return (
+            <Box sx={{flexGrow: 1}}>
+                <AppBar style={{backgroundColor:'#A47C6D'}} position="static">
 
-                    <Button  color="inherit" variant="h6" component="div" >
-                        <a href="/host">
-                            host pocker
-                        </a>
-                    </Button>
+                    <Toolbar>
+                
+                        <Logo/>
+                        <RightSide/>
+                        <Toggle/>
+                        
+                    </Toolbar>
 
-                    <Button color="inherit">
-                        <a href="/signup">
-                            Signup
-                        </a>
-                    </Button>
-
-                    <Button color="inherit">
-                        <a href="/login">
-                            Login
-                        </a>
-                    </Button>
-
-                </Toolbar>
-
-            </AppBar>
-        </Box>
-    )
+                </AppBar>
+            </Box>
+        )
+    }
 }
 
 export default Navbar
